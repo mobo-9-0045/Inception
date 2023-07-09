92be7f6585757378fadef890ab9bacf541b9e80f
mobo-9-0045 <abdelilahoman@gmail.com>
2023-07-09 12:58:49 -0500

    OK

--- srcs/requirement/mariadb/tools/m_db.sh
+++ srcs/requirement/mariadb/tools/m_db.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-service mysql start
+service mariadb start
 
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root' ;" >> db1.sql
-echo "FLUSH PRIVILEGES;" >> db1.sql
-
-mysql < db1.sql
-
-kill $(cat /var/run/mysqld/mysqld.pid)
-
-mysqld --bind-address=0.0.0.0
--- srcs/requirement/wordpress/tools/wp.sh
+++ srcs/requirement/wordpress/tools/wp.sh
@@ -3,10 +3,7 @@ mkdir -p /run/php
 mkdir -p /var/www/html
 chown www-data:www-data /var/www/html
 
-su www-data -s /bin/bash -c "wp core download --path=/var/www/html"
-# Create wp-config.php file if it doesn't exist
-if [ ! -f /var/www/html/wp-config.php ]; then
-	su www-data -s /bin/bash -c "wp config create --path=/var/www/html --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="$MYSQL_HOST""
-fi
-su www-data -s /bin/bash -c "wp core install --path=/var/www/html --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL"
+wp core download --path=/var/www/html --allow-root
+wp config create --path=/var/www/html --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="$MYSQL_HOST" --allow-root
+wp core install --path=/var/www/html --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL --allow-root
 
