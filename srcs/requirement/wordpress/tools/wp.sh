3fa091b746a60933c2f58f496514247978864bd5
mobo <aomman@student.133.ma>
2023-07-12 09:33:31 -0400

    OK

--- srcs/requirement/wordpress/tools/wp.sh
+++ srcs/requirement/wordpress/tools/wp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir -p /run/php
-# mkdir -p /var/www/html
+mkdir -p /var/www/html
 chown -R www-data:www-data /var/www/html
 
 wp core download --path=/var/www/html --allow-root
-wp core install --path=/var/www/html --url=wp_test.com --title=My_site --admin_user=aomman --admin_password=secret --admin_email=abdelilahoman@hotmail.com --allow-root
+wp core install --path=/var/www/html --url=wp_test.com --title=My_site --admin_user=aomman --admin_password=1234 --admin_email=abdelilahoman@hotmail.com --allow-root
 /usr/sbin/php-fpm7.3 -F -R
